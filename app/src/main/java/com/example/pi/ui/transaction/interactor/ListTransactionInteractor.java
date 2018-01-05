5284893cc4debbcc6cb006dbd14dd3277bcb4ae8
MiguelRJ <miguel.rj96@gmail.com>
2018-01-05 21:09:01 +0100

    ListInteractor creado

---
+++ app/src/main/java/com/example/pi/ui/transaction/interactor/ListTransactionInteractor.java
@@ -0,0 +1,25 @@
+package com.example.pi.ui.transaction.interactor;
+
+import com.example.pi.data.db.repository.TransactionRepository;
+
+/**
+ * Created by
+ * @author Miguel Rodriguez Jimenez
+ * @date 05/01/18
+ *
+ */
+
+public class ListTransactionInteractor implements ListTransactionInteractorInterface {
+
+    private ListTransactionInteractorInterface.OnLoadFinisehdListener listener;
+
+    public ListTransactionInteractor(ListTransactionInteractorInterface.OnLoadFinisehdListener listener){
+        this.listener = listener;
+    }
+
+    @Override
+    public void loadTransaction() {
+        listener.onSucces(TransactionRepository.getInstance().getTransactions());
+    }
+
+}
