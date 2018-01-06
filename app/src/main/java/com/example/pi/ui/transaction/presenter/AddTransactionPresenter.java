816bc08aa0372559f8574de59552616a9d16b1cd
MiguelRJ <miguel.rj96@gmail.com>
2018-01-06 02:07:23 +0100

    AddPresenter creado

---
+++ app/src/main/java/com/example/pi/ui/transaction/presenter/AddTransactionPresenter.java
@@ -0,0 +1,49 @@
+package com.example.pi.ui.transaction.presenter;
+
+import com.example.pi.data.db.model.Transaction;
+import com.example.pi.ui.transaction.contract.AddTransactionContract;
+import com.example.pi.ui.transaction.interactor.AddTransactionInteractor;
+import com.example.pi.ui.transaction.interactor.AddTransactionInteractorInterface;
+
+/**
+ * Created by
+ * @author Miguel Rodriguez Jimenez
+ * @date 06/01/18
+ *
+ */
+
+public class AddTransactionPresenter implements AddTransactionContract.Presenter, AddTransactionInteractorInterface.OnAddTransactionListener {
+
+    private AddTransactionContract.View view;
+    private AddTransactionInteractorInterface interactor;
+
+    public AddTransactionPresenter (AddTransactionContract.View view){
+        this.view = view;
+        interactor = new AddTransactionInteractor();
+    }
+
+    /* implements AddTransactionContract.Presenter */
+    @Override
+    public void OnDestroy() {
+        view = null;
+        interactor = null;
+    }
+
+    @Override
+    public void options(int i, Object o) {
+
+    }
+
+    @Override
+    public void validateTransaction(Transaction transaction) {
+        interactor.validateTransaction(transaction);
+    }
+    /* implements AddTransactionContract.Presenter */
+
+    /* implements AddTransactionInteractorInterface.OnAddTransactionListener */
+    @Override
+    public void onSuccess() {
+        view.showOnSuccess();
+    }
+    /* implements AddTransactionInteractorInterface.OnAddTransactionListener */
+}
