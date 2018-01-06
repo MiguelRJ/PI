f22b9f0f552690fdef18ac4b4360e9b8b100a02c
MiguelRJ <miguel.rj96@gmail.com>
2018-01-06 02:02:30 +0100

    AddContract creado

---
+++ app/src/main/java/com/example/pi/ui/transaction/contract/AddTransactionContract.java
@@ -0,0 +1,21 @@
+package com.example.pi.ui.transaction.contract;
+
+import com.example.pi.data.db.model.Transaction;
+import com.example.pi.ui.base.BasePresenter;
+import com.example.pi.ui.base.BaseView;
+
+/**
+ * Created by
+ * @author Miguel Rodriguez Jimenez
+ * @date 06/01/18
+ *
+ */
+
+public interface AddTransactionContract {
+    interface View extends BaseView {
+        void showOnSuccess();
+    }
+    interface Presenter extends BasePresenter {
+        void validateTransaction(Transaction transaction);
+    }
+}
