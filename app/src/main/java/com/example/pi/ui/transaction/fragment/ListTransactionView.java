4ed66ed9c49bbaa96f15b013f499fe41b0c63469
MiguelRJ <miguel.rj96@gmail.com>
2018-01-06 01:48:50 +0100

    menu order en list transaction

--- app/src/main/java/com/example/pi/ui/transaction/fragment/ListTransactionView.java
+++ app/src/main/java/com/example/pi/ui/transaction/fragment/ListTransactionView.java
@@ -2,6 +2,7 @@ package com.example.pi.ui.transaction.fragment;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -10,6 +11,9 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
@@ -17,7 +21,9 @@ import android.widget.Toast;
 import com.example.pi.R;
 import com.example.pi.adapter.TransactionAdapter;
 import com.example.pi.data.db.model.Transaction;
+import com.example.pi.ui.about.AboutUsActivity;
 import com.example.pi.ui.base.BasePresenter;
+import com.example.pi.ui.prefs.AccountSettingActivity;
 import com.example.pi.ui.transaction.contract.ListTransactionContract;
 import com.example.pi.ui.transaction.presenter.ListTransactionPresenter;
 
@@ -33,9 +39,11 @@ import java.util.List;
 public class ListTransactionView extends Fragment implements ListTransactionContract.View {
 
     public static final String TAG = "ListTransactionView";
+
     public interface ListTransactionListener {
         void addNewTransaction(Bundle bundle);
     }
+
     private ListTransactionView.ListTransactionListener callback;
     private TransactionAdapter adapter;
     private TransactionAdapter.OnItemClickListener listener;
@@ -43,6 +51,7 @@ public class ListTransactionView extends Fragment implements ListTransactionCont
 
     private RecyclerView recyclerView;
     private Toolbar toolbar;
+    private FloatingActionButton fab;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -87,15 +96,18 @@ public class ListTransactionView extends Fragment implements ListTransactionCont
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.view_list_transaction,container,false);
-        FloatingActionButton fab = rootView.findViewById(R.id.fabTransaction);
+
+        fab = rootView.findViewById(R.id.fabTransaction);
         toolbar = rootView.findViewById(R.id.toolbar);
         ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
 
         recyclerView = rootView.findViewById(android.R.id.list);
         recyclerView.setHasFixedSize(true);
+        recyclerView.setNestedScrollingEnabled(true);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),1));
 
-        setHasOptionsMenu(true);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -110,6 +122,33 @@ public class ListTransactionView extends Fragment implements ListTransactionCont
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_activity_transaction, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_order_by_creationDate:
+                adapter = new TransactionAdapter(listener);
+                recyclerView.setAdapter(adapter);
+                return true;
+            case R.id.action_order_by_totalAmount:
+                recyclerView.setAdapter(adapter.orderByAmount());
+                return true;
+            case R.id.action_aboutus:
+                startActivity(new Intent(getActivity().getApplicationContext(), AboutUsActivity.class));
+                break;
+            case R.id.action_preferences:
+                startActivity(new Intent(getActivity().getApplicationContext(), AccountSettingActivity.class));
+                break;
+            default:
+                break;
+        }
+        return false;
     }
 
     /* implements ListTransactionContract.View */
